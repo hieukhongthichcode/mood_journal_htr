11178221e1899632979f7c42e72c42206097a47a
hieukhongthichcode <hieubuinat@gmail.com>
2025-08-20 09:48:07 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -12,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ✅ Middleware (cập nhật để xử lý ảnh lớn)
-app.use(cors());
+app.use(cors({
+  origin: "https://mood-journal-htr.vercel.app", // FE domain Vercel
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json({ limit: '5mb' })); // ✅ Cho phép body JSON lớn
 app.use(express.urlencoded({ extended: true, limit: '5mb' })); // ✅ Cho form data lớn
 
